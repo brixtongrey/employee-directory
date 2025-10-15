8fae6a2c8c650ceb371fd816e1898a90a7878a19
Brixtongrey <brixtonaruda@gmail.com>
2025-10-15 16:29:11 -0600

    feat: initial commit

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import app from "#app";
+
+const express = require("express") // importing the express framework
+const app = express() // create the express app
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
