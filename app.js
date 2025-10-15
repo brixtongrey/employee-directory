d72cddf90c0843945bbea8e0ce28f51adbd078ad
Brixtongrey <brixtonaruda@gmail.com>
2025-10-15 17:08:45 -0600

    feat: added GET /employees/:id

--- app.js
+++ app.js
@@ -8,4 +8,20 @@ app.get("/", (request, response) => {
   response.send("Hello employees!");
 });
 
+// GET /employees
+app.get("/employees", (request, response) => {
+    response.status(200).json(employees);
+});
+
+// GET /employees/:id
+app.get("/employees/:id", (request, response) => {
+    const employeeId = Number(request.params.id);
+    const foundEmployee = employees.find((employee) => employee.id === employeeId);
+
+    if (!foundEmployee) {
+        return response.status(404).send("Employee not found");
+    }
+    response.status(200).json(foundEmployee);
+});
+
 export default app;
